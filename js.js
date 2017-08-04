8e0ef1fa541db668ca90266d62d40e736a79344e
Alexander Krawiec <akrawiec@fb.com>
2017-08-04 13:32:46 -0700

    buncha updates

---
+++ js.js
@@ -0,0 +1,44 @@
+
+(function() {
+  'use strict';
+
+  var repeat = function(n, s) { return new Array(n + 1).join(s); };
+  var counts = [];
+  var output = '\n';
+  var nodeList = document.getElementsByTagName('h3');
+  for (var idx = 0; idx < nodeList.length; idx += 1) {
+    var left = nodeList[idx].textContent + ' ';
+    var right = ' ' + (counts[counts.length] = nodeList[idx].nextElementSibling.getElementsByTagName('li').length);
+    output += left + repeat(20 - left.length - right.length, '.') + right + '\n';
+  }
+  var total = String(counts.reduce(function(a, b) { return a + b; }, 0));
+  output += repeat(20, '=') + '\n';
+  output += repeat(20 - total.length, ' ') + total + '\n';
+  output += '\n';
+  console.log(output);
+
+  var largeCard = document.getElementById('large-card');
+  document.body.addEventListener('mouseover', function(event) {
+    if (event.target.tagName === 'IMG') {
+      largeCard.alt = event.target.alt;
+      largeCard.src = event.target.src;
+    }
+  }, false);
+}());
+
+
+//Alex's work
+
+let searchPopup = document.querySelector('#search-popup');
+let searchLink = document.querySelector('#search-link');
+let popupCloser = document.querySelector('#popup-closer');
+
+//make search button display popup div
+searchLink.addEventListener('click', () => {
+  searchPopup.classList.remove('hidden');
+});
+
+//make closing button in popup div hide the div
+popupCloser.addEventListener('click', () => {
+  searchPopup.classList.add('hidden');
+});
